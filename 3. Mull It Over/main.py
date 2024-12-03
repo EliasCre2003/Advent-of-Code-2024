710e365d514273d0029bd398697e9ffa0c999156
Elias Cregård <eliascregard701@gmail.com>
2024-12-03 08:51:20 +0100

    Day 3 done

---
+++ 3. Mull It Over/main.py
@@ -0,0 +1,45 @@
+import re
+
+def get_all_mull(text: str) -> list[str]:
+    return re.findall(r"mul\(\d+,\d+\)", text)
+
+def remove_first_occurence(string: str, regex: str) -> tuple[str, str]:
+    match = re.search(regex, string)
+    if match:
+        return match.group(), re.sub(regex, '', string, count=1)
+    return match, string
+
+def parse_mull(string: str) -> tuple[int, int]:
+    string = string.removeprefix("mul(")
+    string = string.removesuffix(")")
+    string = string.split(",")
+    return int(string[0]), int(string[1])
+
+def part1(lines: list[str]) -> str:
+    string = ''.join(line.strip() for line in lines)
+    mulls = [parse_mull(mull) for mull in get_all_mull(string)]
+    return sum(mull[0] * mull[1] for mull in mulls)
+
+def part2(lines: list[str]) -> str:
+    string = ''.join(line.strip() for line in lines)
+    mull = True
+    total = 0
+    while string:
+        term, string = remove_first_occurence(string, r"mul\(\d+,\d+\)|do\(\)|don't\(\)")
+        if not term: break
+        if term == "do()": mull = True
+        elif term == "don't()": mull = False
+        elif mull:
+            mull = parse_mull(term)
+            total += mull[0] * mull[1]
+    return total
+
+def main():
+    with open("3. Mull It Over/input.txt", 'r') as f:
+        lines = f.readlines()
+
+    for i, part in enumerate([part1, part2]):
+        print(f"Part {i+1}: {part(lines)}")
+
+if __name__ == "__main__":
+    main()
