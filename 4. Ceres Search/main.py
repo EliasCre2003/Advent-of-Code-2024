6f950d29db60d9d53aafd1bbe17d9b9a2f5f9f8a
Elias Cregård <eliascregard701@gmail.com>
2024-12-04 19:25:15 +0100

    Rewrote it into a more readable form

--- 4. Ceres Search/main.py
+++ 4. Ceres Search/main.py
@@ -1,72 +1,96 @@
 from copy import deepcopy
+from collections.abc import Callable
 
-def search1(lines: list[list[str]], origin: tuple[int, int]):
-    letter = lines[origin[0]][origin[1]]
-    occurence = 0
-    if letter == 'X':
-        sequence = ['M', 'A', 'S']
-    elif letter == 'S':
-        sequence = ['A', 'M', 'X']
-    else:
-        return 0
-
-    for dir in [0,1], [1, 0], [1, -1], [1, 1]:
-        point = origin
-        for i in range(3):
-            point = point[0] + dir[0], point[1] + dir[1]
-            if not (0 <= point[0] < len(lines)) or not (0 <= point[1] < len(lines[1])):
-                break
-            if lines[point[0]][point[1]] != sequence[i]:
-                break
-        else:
-            occurence += 1
-    return occurence
-
-def search2(lines: list[list[str]], origin: tuple[int, int]):
-    letter = lines[origin[0]][origin[1]]
-    sequences = {'M': ['A', 'S'],
-                 'S': ['A', 'M']}
-    if letter not in sequences:
-        return 0
-    sequence = sequences[letter]
-    if (origin[1] >= (len(lines[0]) - 2) or (origin[0] >= (len(lines) - 2))):
-        return 0 
-    if not (lines[origin[0]+1][origin[1]+1] == sequence[0] and
-        lines[origin[0]+2][origin[1]+2] == sequence[1]):
-        return 0
-    letter = lines[origin[0]][origin[1] + 2]
-    if letter not in sequences:
-        return 0
-    sequence = sequences[letter]
-    if not (lines[origin[0]+1][origin[1]+1] == sequence[0] and
-        lines[origin[0]+2][origin[1]] == sequence[1]):
-        return 0
-    return 1
+class FieldPattern:
     
+    def __init__(self, coords: list[tuple[int, int]]):
+        self.coords = coords.copy()
+
+
+class LetterSequence:
     
-
-def part1(lines: list[list[str]]):
-    occurenses = 0
-    for i, line in enumerate(lines):
-        for j, _ in enumerate(line):
-            occurenses += search1(lines, (i, j))
-    return occurenses
-        
+    def __init__(self, letter: list[str]):
+        self.letters = letter.copy()
+    
+    def match(self, sequence: 'LetterSequence') -> bool:
+        return self.letters == sequence.letters
 
 
-def part2(lines):
-    occurenses = 0
-    for i, line in enumerate(lines):
-        for j, _ in enumerate(line):
-            occurenses += search2(lines, (i, j))
-    return occurenses
+class LetterField:
+    
+    def __init__(self, letters: list[list[str]]):
+        self.letters = deepcopy(letters)
+    
+    def get_at(self, coord: tuple[int, int]) -> str:
+        try:
+            return self.letters[coord[1]][coord[0]]
+        except IndexError:
+            return '.' 
+
+    def get_sequence(self, offset_coord: tuple[int, int], pattern: FieldPattern):
+        return LetterSequence([
+            self.get_at((coord[0] + offset_coord[0], coord[1] + offset_coord[1])) 
+            for coord in pattern.coords
+        ])
+    
+    def scan(self, finder_function: Callable[['LetterField', tuple[int, int]], int]) -> int:
+        occurenses = 0
+        for y, line in enumerate(self.letters):
+            for x in range(len(line)):
+                occurenses += finder_function(self, (x, y))
+        return occurenses
+
+
+def part1(field: LetterField, origin: tuple[int, int]) -> int:
+    letter = field.get_at(origin)
+    try:
+        correct_sequence = {
+            'X': LetterSequence(['M', 'A', 'S']),
+            'S': LetterSequence(['A', 'M', 'X'])
+        }[letter]
+    except KeyError:
+        return 0
+    patterns = [
+        FieldPattern([(0,  1), (0, -2), (0,  3)]),
+        FieldPattern([(1,  0), (2, -0), (3,  0)]),
+        FieldPattern([(1,  1), (2, -2), (3,  3)]),
+        FieldPattern([(1, -1), (2, -2), (3, -3)])
+    ]
+    return sum(
+        field.get_sequence(origin, pattern).match(correct_sequence)
+        for pattern in patterns
+    )
+
+
+def part2(field: LetterField, origin: tuple[int, int]) -> int:
+    letters = [field.get_at(origin),
+               field.get_at((origin[0] + 2, origin[1]))]
+    correct_sequences = {
+        'M': LetterSequence(['A', 'S']),
+        'S': LetterSequence(['A', 'M'])
+    }
+    patterns = [
+        FieldPattern([(1, 1), (2, 2)]),
+        FieldPattern([(1, 1), (0, 2)])
+    ]
+    try:
+        sequences = [correct_sequences[letter] for letter in letters]
+    except KeyError:
+        return 0
+
+    match1 = field.get_sequence(origin, patterns[0]).match(sequences[0]) #  \
+    match2 = field.get_sequence(origin, patterns[1]).match(sequences[1]) #  /
+    return match1 and match2  #   \ + / = X 
+
 
 def main():
     with open("4. Ceres Search/input.txt", 'r') as f:
         lines = f.readlines()
     input = [list(line.strip()) for line in lines]
+    field = LetterField(input)
     for i, part in enumerate([part1, part2]):
-        print(f"Part {i+1}: {part(deepcopy(input))}")
+        print(f"Part {i+1}: {field.scan(part)}")
+
 
 if __name__ == "__main__":
     main()
