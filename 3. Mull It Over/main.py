e2bdbc104ad5efca5f75343440d7d759d1a468bc
Elias Cregård <eliascregard701@gmail.com>
2024-12-04 14:53:26 +0100

    Simplified part 2

--- 3. Mull It Over/main.py
+++ 3. Mull It Over/main.py
@@ -1,13 +1,4 @@
-import re
-
-def get_all_mull(text: str) -> list[str]:
-    return re.findall(r"mul\(\d+,\d+\)", text)
-
-def remove_first_occurence(string: str, regex: str) -> tuple[str, str]:
-    match = re.search(regex, string)
-    if match:
-        return match.group(), re.sub(regex, '', string, count=1)
-    return match, string
+from re import findall
 
 def parse_mull(string: str) -> tuple[int, int]:
     string = string.removeprefix("mul(")
@@ -16,28 +7,31 @@ def parse_mull(string: str) -> tuple[int, int]:
     return int(string[0]), int(string[1])
 
 def part1(input: str) -> str:
-    mulls = [parse_mull(mull) for mull in get_all_mull(input)]
+    regex = r"mul\(\d+,\d+\)"
+    mulls = [parse_mull(mull) for mull in findall(regex, input)]
     return sum(mull[0] * mull[1] for mull in mulls)
 
 def part2(input: str) -> str:
     mull = True
     total = 0
-    while input:
-        term, input = remove_first_occurence(input, r"mul\(\d+,\d+\)|do\(\)|don't\(\)")
-        if not term: break
-        if term == "do()": mull = True
-        elif term == "don't()": mull = False
-        elif mull:
-            mull = parse_mull(term)
-            total += mull[0] * mull[1]
+    regex = r"mul\(\d+,\d+\)|do\(\)|don't\(\)"
+    stream = findall(regex, input)
+    for term in stream:
+        match term:
+            case "do()": mull = True
+            case "don't()": mull = False
+            case _:
+                if not mull: continue
+                factors = parse_mull(term)
+                total += factors[0] * factors[1]
     return total
 
 def main():
     with open("3. Mull It Over/input.txt", 'r') as f:
-        lines = f.read()
+        input = f.read()
 
     for i, part in enumerate([part1, part2]):
-        print(f"Part {i+1}: {part(lines)}")
+        print(f"Part {i+1}: {part(input)}")
 
 if __name__ == "__main__":
     main()
