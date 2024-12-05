3081634d300291419acc5f7505a29475edc2525b
Elias Cregård <eliascregard701@gmail.com>
2024-12-05 22:32:43 +0100

    More clean code now

--- 5. Print Queue/main.py
+++ 5. Print Queue/main.py
@@ -10,25 +10,63 @@ class OrderingMap:
     def get_all_not_allowed_before(self, key_val: int) -> set[int]:
         return self.vals[key_val]
     
-    def is_in(self, num: int) -> bool:
+    def __contains__(self, num: int) -> bool:
         return num in self.vals
+    
+    def get(self, key_val: int) -> set[int]:
+        return self.vals[key_val] if key_val in self.vals else set()
+
+    def __getitem__(self, key: int) -> set[int]:
+        return self.vals[key] if key in self.vals else set()
 
 
 class NumSequence:
     def __init__(self, vals: list[int]):
         self.vals = vals
+        self.dfs_cache = {}
+
+    def __hash__(self) -> int:
+        return tuple(self.vals).__hash__()
+    
+    def __contains__(self, val: int) -> bool:
+        return val in self.vals
+    
+    def __getitem__(self, index: int) -> int:
+        return self.vals[index]
+    
+    def __len__(self) -> int:
+        return len(self.vals)
     
     def is_ordered(self, ordering: OrderingMap) -> bool:
         occured: set[int] = set()
-        # for num in self.vals:
-        #     if not (
-        #         occured and
-        #         ordering.is_in(num) and
-                
-        #         ):
+        for num in self.vals:
+            if (not occured or
+                num not in ordering or
+                not occured.intersection(ordering.get(num))
+                ): occured.add(num)
+            else: return False
+        return True
+
+    def get_all_required_after(self, ordering: OrderingMap, key) -> set[int]:
+        if key in self.dfs_cache:
+            return self.dfs_cache[key]
+        if key not in ordering:
+            return set()
+        afters: set[int] = ordering[key]
+        for new_key in afters:
+            if new_key not in self: continue
+            afters = afters.union(self.get_all_required_after(ordering, new_key))
+        self.dfs_cache[key] = afters
+        return afters
+    
+
+    def correct_order(self, ordering: OrderingMap) -> None:
+        self.dfs_cache = {}
+        lengths = {k: len(self.get_all_required_after(ordering, k)) for k in self.vals}
+        self.vals = list(reversed(sorted(self.vals, key=lambda x: lengths[x])))
 
 
-def parse_first(lines: list[str]) -> dict[int: set[int]]:
+def parse_map(lines: list[str]) -> OrderingMap:
     ordering = {}
     for line in lines:
         pair = line.strip().split('|')
@@ -37,32 +75,18 @@ def parse_first(lines: list[str]) -> dict[int: set[int]]:
             ordering[num1].add(num2)
         else:
             ordering[num1] = {num2}
-    return ordering
+    return OrderingMap(ordering)
 
-def parse_second(lines: list[str]) -> list[list[str]]:
-    return [[int(num) for num in line.strip().split(",")] 
+def parse_sequences(lines: list[str]) -> list[NumSequence]:
+    return [NumSequence([int(num) for num in line.strip().split(",")] )
             for line in lines]
 
 
-def part1(ordering: dict[int: set[int]], updates: list[list[int]]) -> int:
+def part1(ordering: OrderingMap, sequences: list[NumSequence]) -> int:
     total = 0
-    to_remove = []
-    for update in updates:
-        occured: set[int] = set()
-        for num in update:
-            if len(occured) == 0:
-                occured.add(num)
-            elif num not in ordering:
-                occured.add(num)
-            elif len(occured.intersection(ordering[num])) == 0:
-                    occured.add(num)
-            else:
-                break
-        else:
-            total += update[len(update) // 2]
-            to_remove.append(update)
-    for update in to_remove:
-        updates.remove(update)
+    for sequence in sequences:
+        if sequence.is_ordered(ordering):
+            total += sequence[len(sequence) // 2]
     return total
 
 
@@ -78,18 +102,18 @@ def dfs(ordering: dict[int: set[int]], key: int, update: list[int], cache: dict[
     cache[(key, tuple(update))] = afters
     return afters
 
-
-def part2(ordering: dict[int: set[int]], updates: list[list[int]]) -> int:
+def part2(ordering: OrderingMap, sequences: list[NumSequence]) -> int:
     total = 0
-    for update in updates:
-        lens = {k: len(dfs(ordering, k, update)) for k in update}
-        update = list(reversed(sorted(update, key=lambda x: lens[x])))
-        total += update[len(update) // 2]
+    for sequence in sequences:
+        if sequence.is_ordered(ordering): continue
+        sequence.correct_order(ordering)
+        total += sequence[len(sequence) // 2]
     return total
 
 
+
 def main():
-    with open("5. Print Queue/test.txt", 'r') as f:
+    with open("5. Print Queue/input.txt", 'r') as f:
         lines = f.readlines()
     
     sections = []
@@ -97,16 +121,13 @@ def main():
         if line == '\n':
             sections.append(lines[:i])
             sections.append(lines[i+1:])
-    sections = (parse_first(sections[0]), parse_second(sections[1]))
+    sections = (parse_map(sections[0]), parse_sequences(sections[1]))
+    ordering = OrderingMap(sections[0])
+    sequences = [NumSequence(sequence) for sequence in sections[1]]
 
-    print(part1(*sections))
-    print(part2(*sections))
+    for i, part in enumerate([part1, part2]):
+        print(f"Part {i+1}: {part(ordering, sequences)}")
 
-    # for i, part in enumerate([part1, part2]):
-    #     print(f"Part {i+1}: {part(parse_first(sections[0]), parse_second(sections[1]))}")
-
-    # for i in range(len(sections[1][3])):
-    #     print(len(dfs(sections[0], sections[1][3][i], sections[1][3])))
 
 if __name__ == "__main__":
     main()
