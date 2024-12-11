2cb3690eb22d5eb6df200042192801a2a7fb2895
Elias Cregård <eliascregard701@gmail.com>
2024-12-11 19:25:15 +0100

    Part 2 done!

--- 11. Plutonian Pebbles/main.py
+++ 11. Plutonian Pebbles/main.py
@@ -1,35 +1,37 @@
 
-def split_stones(stones: list[int]) -> list[int]:
-    new_stones = []
-    for stone in stones:
-        if stone == 0:
-            new_stones.append(1)
-        elif len(str_stone := str(stone)) % 2 == 0:
-            half_length = len(str_stone) // 2
-            new_stones.append(int(str_stone[:half_length]))
-            new_stones.append(int(str_stone[half_length:]))
-        else:
-            new_stones.append(stone * 2024)
-    return new_stones
-
-def split_stones_n_times(stones: list[int], n: int) -> list[int]:
-    for _ in range(n):
-        stones = split_stones(stones)
-    return stones
-
-def part1(stones: list[int]) -> int:
-    return len(split_stones_n_times(stones, 25))    
+def split_stone(stone: int) -> list[int]:
+    if stone == 0:
+        return [1]
+    elif len(str_stone := str(stone)) % 2 == 0:
+        half_length = len(str_stone) // 2
+        return [
+            int(str_stone[:half_length]),
+            int(str_stone[half_length:])
+        ]
+    else:
+        return [stone * 2024]
 
 
-def part2() -> int:
-    ...
+def width_at_depth(stone: int, target_depth: int, cache: dict[tuple[int, int], int], current_depth: int = 0):
+    if target_depth == current_depth:
+        return 1
+    if (stone, current_depth) in cache:
+        return cache[(stone, current_depth)]
+    new_stones = split_stone(stone)
+    result = sum(
+        width_at_depth(stone, target_depth, cache, current_depth + 1) 
+        for stone in new_stones
+    )
+    cache[(stone, current_depth)] = result
+    return result
 
 
 def main():
     with open('11. Plutonian Pebbles/input.txt', 'r') as f:
         stones = [int(n) for n in f.read().strip().split(' ')]
+    for i, depth in enumerate([25, 75]):
+        print(f"Part {i+1}: {sum(width_at_depth(stone, depth, cache={}) for stone in stones)}")
 
-    print(part1(stones))
 
 if __name__ == "__main__":
     main()
