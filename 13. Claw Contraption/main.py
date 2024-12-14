93d54fd1c2880cd6c189f715c4f7cc128b969959
Elias Cregård <eliascregard701@gmail.com>
2024-12-14 12:32:52 +0100

    Minor optimization

--- 13. Claw Contraption/main.py
+++ 13. Claw Contraption/main.py
@@ -1,5 +1,6 @@
 from re import findall
 
+
 class ClawMachine:
     def __init__(self, a: tuple[int, int], b: tuple[int, int], prize: tuple[int, int]):
         self.a = a
@@ -7,11 +8,11 @@ class ClawMachine:
         self.prize = prize
     
     def tokens_required(self) -> int:
-        b_clicks = -((-self.a[1]*self.prize[0] + self.a[0]*self.prize[1]) / 
+        b_clicks = ((self.a[1]*self.prize[0] - self.a[0]*self.prize[1]) / 
                      (self.a[1]*self.b[0] - self.a[0]*self.b[1]))
         a_clicks = (self.prize[0] - self.b[0] * b_clicks) / self.a[0]
         n_tokens = a_clicks * 3 + b_clicks
-        if int(n_tokens) == n_tokens: return int(n_tokens)
+        if n_tokens.is_integer(): return int(n_tokens)
         else: return 0
    
 
@@ -22,16 +23,16 @@ def parse_input(lines: list[str], error: int = 0) -> list[ClawMachine]:
         b_button = findall(r'Button B: X\+(\d+), Y\+(\d+)', lines[i+1])[0]
         prize = findall(r'Prize: X=(\d+), Y=(\d+)', lines[i+2])[0]
         machines.append(ClawMachine(
-            (int(a_button[0]), int(a_button[1])), 
+            (int(a_button[0]), int(a_button[1])),
             (int(b_button[0]), int(b_button[1])),
             (int(prize[0]) + error, int(prize[1]) + error)
         ))
     return machines
 
+
 def main():
     with open("13. Claw Contraption/input.txt", 'r') as f:
         lines = f.readlines()
-
     for i, error in enumerate([0, 10000000000000]):
         machines = parse_input(lines, error)
         result = sum(machine.tokens_required() for machine in machines)
