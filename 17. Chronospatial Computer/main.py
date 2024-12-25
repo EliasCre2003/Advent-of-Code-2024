310a5c7d9f68e53bcb61f794e9f9ffc99628e67c
Elias Cregård <eliascregard701@gmail.com>
2024-12-25 01:28:05 +0100

    Part 2 finally

--- 17. Chronospatial Computer/main.py
+++ 17. Chronospatial Computer/main.py
@@ -24,10 +24,10 @@ class CPU:
                 cpu: CPU = replace(self, rb = self.rb ^ operand, ip = self.ip + 2)
             case 2:
                 cpu: CPU = replace(self, rb = self.combo(operand) % 8, ip = self.ip + 2)
-            case 3:
+            case 3: 
                 cpu: CPU = replace(self, ip = operand if self.ra != 0 else self.ip + 2)
             case 4:
-                cpu: CPU = replace(self, rb = self.rb ^ self. rc, ip = self.ip + 2)
+                cpu: CPU = replace(self, rb = self.rb ^ self.rc, ip = self.ip + 2)
             case 5:
                 cpu: CPU = replace(self, output = self.output + [self.combo(operand) % 8], ip = self.ip + 2)
             case 6:
@@ -54,37 +54,72 @@ class CPU:
     
     def current_output(self) -> str:
         return ','.join([str(n) for n in self.output])
-        
-    
+
+
+def reverse_engineer(output: list[int]) -> int:
+    a = 0
+    for out in reversed(output):
+        for i in range(0, 8):
+            temp_a = a * 8 + i
+            if decompiled_iteration(temp_a) == out:
+                a = temp_a
+                break  
+    return a
+
+def decompiled_iteration(a: int) -> int:
+    b = (a % 8) ^ 0b010
+    c = a // (2**b)
+    b = (b ^ c) ^ 0b111
+    return b % 8
+
 def parse_input(string: str) -> CPU:
     ra, rb, rc = map(int, findall(r"Register .: (\d+)", string))
     program = [int(n) for n in search(r"Program:\s*([\d+,]+)", string)
                .group(1).split(',')]
     return CPU(ra, rb, rc, program, 0, [])
-    
+
 
 def part1(cpu: CPU) -> str:
     cpu = cpu.run()
     return cpu.current_output()
 
-def part2(cpu: CPU) -> int:
-    # a = 97653057
-    a = 0
-    while True:
-        # print(a)
-        new_cpu = replace(cpu, ra = a)
-        new_cpu = new_cpu.run()
-        if new_cpu.program == new_cpu.output:
-            return a
-        print(new_cpu.output)
-        a += 1
+
+def part2() -> int:
+    data = [2,4,1,2,7,5,4,5,0,3,1,7,5,5,3,0]
+    return reverse_engineer(data)
+
 
 def main():
     with open("17. Chronospatial Computer/input.txt", 'r') as f:
         data = f.read()
     cpu = parse_input(data)
-    print(part2(cpu))
+    for i, result in enumerate([part1(cpu), part2()]):
+        print(f'Part {i+1}: {result}')
 
 
 if __name__ == '__main__':
     main()
+
+
+# def dissasemble(program: list[int]) -> str:
+    
+#     def combo(operand: int) -> str:
+#         if 0 <= operand <= 3: return str(operand)
+#         if operand == 4: return 'A'
+#         if operand == 5: return 'B'
+#         if operand == 6: return 'C'
+
+#     code = ''
+#     for ip in range(0, len(program), 2):
+#         opcode = program[ip]
+#         operand = program[ip + 1]
+#         match opcode:
+#             case 0: code += f'adv {combo(operand)}\n'
+#             case 1: code += f'bxl {operand}\n'
+#             case 2: code += f'bst {combo(operand)}\n'
+#             case 3: code += f'jnz {operand}\n'
+#             case 4: code += f'bxc\n'
+#             case 5: code += f'out {combo(operand)}\n'
+#             case 6: code += f'bdv {combo(operand)}\n'
+#             case 7: code += f'cdv {combo(operand)}\n'
+#     return code
