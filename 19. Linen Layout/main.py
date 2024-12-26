5c59f6e1257553d631dfcaabb6fd9066bf3d3ae7
Elias Cregård <eliascregard701@gmail.com>
2024-12-26 15:04:56 +0100

    Optimized and simplified code

--- 19. Linen Layout/main.py
+++ 19. Linen Layout/main.py
@@ -1,22 +1,5 @@
-from enum import Enum
-
-
-class Stripe(Enum):
-    WHITE = 'w'
-    BLUE = 'u'
-    BLACK = 'b'
-    RED = 'r'
-    GREEN = 'g'
-
-    def __str__(self) -> str:
-        return self.value
-    
-    def __repr__(self) -> str:
-        return str(self)
-
-
 class StripeSequence:
-    def __init__(self, sequence: list[Stripe]):
+    def __init__(self, sequence: list[str]):
         self.sequence = sequence
 
     def match(self, sequence: 'StripeSequence') -> bool:
@@ -26,12 +9,6 @@ class StripeSequence:
     def __len__(self) -> int:
         return len(self.sequence)
 
-    def __str__(self):
-        return ''.join(map(str, self.sequence))
-    
-    def __repr__(self):
-        return str(self)
-    
     def __eq__(self, other: 'StripeSequence') -> bool:
         return self.sequence == other.sequence
     
@@ -43,7 +20,7 @@ class StripeSequence:
 
 
 def to_stripe_sequence(string: str) -> StripeSequence:
-    return StripeSequence([Stripe(char) for char in string])
+    return StripeSequence([char for char in string])
 
 
 def parse_input(data: str) -> tuple[list[StripeSequence], list[StripeSequence]]:
@@ -51,17 +28,6 @@ def parse_input(data: str) -> tuple[list[StripeSequence], list[StripeSequence]]:
     towels = [to_stripe_sequence(string) for string in towel_str.split(', ')]
     designs = [to_stripe_sequence(line.strip()) for line in pattern_str.split('\n')]
     return towels, designs
-
-
-def is_possible(design: StripeSequence, towels: list[StripeSequence], cache: dict[StripeSequence, bool] = {}) -> bool:
-    if len(design) == 0: return True
-    if design in cache: return cache[design]
-    for towel in towels:
-        if not design.match(towel): continue
-        if is_possible(design[len(towel):], towels):
-            return True
-    cache[design] = False
-    return False
 
 
 def possible_combinations(design: StripeSequence, towels: list[StripeSequence], cache: dict[StripeSequence, int] = {}) -> int:
@@ -75,20 +41,13 @@ def possible_combinations(design: StripeSequence, towels: list[StripeSequence], 
     return total
 
 
-def part1(towels: list[StripeSequence], desings: list[StripeSequence]) -> int:
-    return sum(is_possible(design, towels) for design in desings)
-
-
-def part2(towels: list[StripeSequence], desings: list[StripeSequence]) -> int:
-    return sum(possible_combinations(design, towels) for design in desings)
-
-
 def main():
     with open('19. Linen Layout/input.txt', 'r') as f:
         data = f.read()
     towels, designs = parse_input(data)
-    for i, part in enumerate([part1, part2]):
-        print(f'Part {i+1}: {part(towels, designs)}')
+    combination_map = {design: possible_combinations(design, towels) for design in designs}
+    print(f'Part 1: {sum(n > 0 for n in combination_map.values())}\n'
+          f'Part 2: {sum(n for n in combination_map.values())}')
 
 
 if __name__ == '__main__':
