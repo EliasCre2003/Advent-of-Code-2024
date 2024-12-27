f2cf18c356b041efd21fa3c301e6d9e6a5980516
Elias Cregård <eliascregard701@gmail.com>
2024-12-27 16:54:54 +0100

    day 20 done

--- 20. Race Condition/main.py
+++ 20. Race Condition/main.py
@@ -1,5 +1,3 @@
-from sys import setrecursionlimit
-
 class Coord:
     def __init__(self, x: int, y: int):
         self.x = x
@@ -14,13 +12,15 @@ class Coord:
     def __mul__(self, other) -> 'Coord':
         if type(other) is Coord:
             return Coord(self.x * other.x, self.y * other.y)
-        elif type(other) is int:
+        if type(other) is int:
             return Coord(self.x * other, self.y * other)
+        raise ValueError(f'Cannot multiply a Coord with a {type(other)}')
     
     def __hash__(self):
         return hash((self.x, self.y))
     
     def __eq__(self, value: 'Coord') -> bool:
+        if type(value) is not Coord: return False
         return self.x == value.x and self.y == value.y
     
     def __str__(self) -> str:
@@ -28,69 +28,74 @@ class Coord:
 
     def __repr__(self) -> str:
         return str(self)
-        
+
 
 class Cheat:
     def __init__(self, start_point: Coord, end_point: Coord, time_saved: int):
         self.start_point = start_point
         self.end_point = end_point
         self.time_saved = time_saved
+    
+    def __eq__(self, value):
+        return (self.start_point == value.start_point and
+                self.end_point == value.end_point and
+                self.time_saved == value.time_saved)
 
-    
-    
+    def __hash__(self):
+        return hash((self.start_point, self.end_point, self.time_saved))
+
 
 class Track:
     def __init__(self, track_coords: list[Coord]):
         self.track_times = {coord: i for i, coord in enumerate(track_coords)}
 
-    def generate_cheats(self) -> list[Cheat]:
-        cheats: list[Cheat] = []
+    def generate_cheats(self, cheat_time: int) -> set[Cheat]:
+        cheats: set[Cheat] = set()
         for coord, time in self.track_times.items():
-            for dir in [Coord(0, 1), Coord(1, 0), Coord(0, -1), Coord(-1, 0)]:
-                if (other_coord := coord + dir * (cheat_len := 1)) in self.track_times: pass
-                elif (other_coord := coord + dir * (cheat_len := 2)) in self.track_times: pass
-                else: continue
-                cheats.append(Cheat(coord, other_coord, self.track_times[other_coord] - time - cheat_len))
+            for x_offset in range(0, cheat_time+1):
+                for y_offset in range(0, cheat_time+1 - x_offset):
+                    for mul in [Coord(1, 1), Coord(-1, 1), Coord(1, -1), Coord(-1, -1)]:
+                        if (end_coord := coord + Coord(x_offset, y_offset) * mul) in self.track_times:
+                            cheats.add(Cheat(coord, end_coord, self.track_times[end_coord] - time - (x_offset+y_offset)))             
         return cheats
-
-
 
 
 def parse_input(lines: list[str]) -> Track:
     for y, line in enumerate(lines):
-        start_coord = False
+        coord = False
         for x, cell in enumerate(line):
             if cell == 'S': 
-                start_coord = Coord(x, y)
+                coord = Coord(x, y)
                 break
-        if start_coord is not False: break
-
-    track_coords: list[Coord] = [] 
-    def inner(coord: Coord) -> bool:
-        if lines[coord.y][coord.x] == '#' or coord in track_coords: return False
+        if coord is not False: break
+    previous_coord = None
+    track_coords: list[Coord] = []
+    while True:
         track_coords.append(coord)
-        if lines[coord.y][coord.x] == 'E': return True
+        if lines[coord.y][coord.x] == 'E': break
         for offset_coord in [Coord(0, 1), Coord(1, 0), Coord(0, -1), Coord(-1, 0)]:
-            if inner(coord + offset_coord): return True
-        return False
-    inner(start_coord)
+            new_coord = coord + offset_coord
+            if lines[new_coord.y][new_coord.x] == '#' or new_coord == previous_coord: continue
+            previous_coord = coord
+            coord = new_coord
+            break
     return Track(track_coords)
 
 
 def part1(track: Track) -> int:
-    return sum(cheat.time_saved >= 100 for cheat in track.generate_cheats())
+    return sum(cheat.time_saved >= 100 for cheat in track.generate_cheats(2))
 
 
-def part2(track: Track):
-    ...
+def part2(track: Track) -> int:
+    return sum(cheat.time_saved >= 100 for cheat in track.generate_cheats(20))
 
 
 def main():
-    setrecursionlimit(999999)
     with open('20. Race Condition/input.txt', 'r') as f:
         lines = f.readlines()
     track = parse_input(lines)
-    print(part1(track))
+    for i, part in enumerate([part1, part2]):
+        print(f'Part {i+1}: {part(track)}')
 
 
 if __name__ == '__main__':
